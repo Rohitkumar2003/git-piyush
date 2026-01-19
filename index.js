01c25c7825cf0896a56aa2b6e2abb7dabb2bca55
Rohit Kumar <Mr.developer2003@gmail.com>
2026-01-19 22:22:44 +0530

    Intial Code Added

---
+++ index.js
@@ -0,0 +1,10 @@
+const names = 'Rohit';
+const emailid  = 'Mr.developer2003@gmail.com';
+const mobileNumber = 9818472776;
+
+
+const user1 = (names,emailid,mobileNumber)=>{ 
+    console.log(`Hey ${names} Your Email id ${emailid} and Your Mobile Number ${mobileNumber}`);
+}
+
+ user1(names,mobileNumber,emailid)
