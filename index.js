bf21fd99f11aa46f5221ccbd1c84e841bf7185b8
Rohit Kumar <Mr.developer2003@gmail.com>
2026-01-19 22:30:28 +0530

    Added isAbove18 fn

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
-const names = 'Rohit';
+const userName = 'Rohit';
 const emailid  = 'Mr.developer2003@gmail.com';
 const mobileNumber = 9818472776;
+const age = '10/08/2003';
 
-
-const user1 = (names,emailid,mobileNumber)=>{ 
-    console.log(`Hey ${names} Your Email id ${emailid} and Your Mobile Number ${mobileNumber}`);
+const user1 = (userName,emailid,mobileNumber)=>{ 
+    console.log(`Hey ${userName} Your Email id ${emailid} and Your Mobile Number ${mobileNumber}`);
 }
 
- user1(names,mobileNumber,emailid)
+ user1(userName,mobileNumber,emailid)
+
+ const isAbove18 = () => {
+    return age >=18 
+ }
+
+
